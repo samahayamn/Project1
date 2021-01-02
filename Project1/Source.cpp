6dc55a157a6b134e58031cb962af54a57a883bba
Maryam Hussein <201900466@pua.edu.eg>
2021-01-02 13:30:16 +0200

    Add project files.

---
+++ Project1/Source.cpp
@@ -0,0 +1,21 @@
+﻿#include<stdio.h>
+
+void main() {
+	/*int z;
+	z =  5 + 3 * 4 / (2 + 4) / 2 ;
+	printf(" z=\n %d", z);*/
+
+
+	/*int y;
+	y = 3 * 9 *(3 + (9 * 3 / (3)));
+	printf(" y=\n %d", y);*/
+
+	int x;
+	int m;
+	scanf_s("x= %d", &x);
+	m = 2 * x;
+	printf("m=\n %d", m);
+
+	
+}
+
